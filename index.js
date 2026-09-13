25902d4c8283578faa477ec010f5a5bb6efb7e71
KurobeerSabasand <k.abe.6342@gmail.com>
2026-09-13 17:38:59 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ app.use(
     origin: [
       "http://localhost:5173", // 開発環境
       "https://kurobeersabasand.github.io",
+      "https://wms-frontend-qvodjk5lk-kurobeer.vercel.app"
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
